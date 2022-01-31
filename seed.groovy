3252ab815546972ec98878e05f78985c7c313323
susarlarayudu <rayuduroyal1996@gmail.com>
2022-01-31 10:51:05 +0530

    auto pushing chainges repo

--- seed.groovy
+++ seed.groovy
@@ -213,4 +213,23 @@ pipelineJob('Helm-Deploy') {
     }
 }
 
-
+pipelineJob('K8s-DB-Deploy') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/rayuduroyal/Jenkins.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile-all-components')
+            'lightweight'(true)
+        }
+    }
+}
